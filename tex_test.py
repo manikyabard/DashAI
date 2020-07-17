2b7509c7327b8cb1e95da6b1c9aefd09cdae7b04
manikyabard <manikyabard@gmail.com>
2020-07-17 21:18:48 +0000

    text default models

--- tex_test.py
+++ tex_test.py
@@ -1,4 +1,4 @@
-from text.databunch import DashTextDatabunch
+from text.learner import DashTextLearner
 import json
 import torch.optim
 import path
@@ -6,8 +6,9 @@ import path
 path = path.Path('./')
 with open('./data/response_new.json') as f:
 			response = json.load(f)
-databunch = DashTextDatabunch.create_text_databunch(response)
-print('created databunch')
-print(databunch)
+learn = DashTextLearner.create_text_learner(response)
+print('created learner')
+
+learn.fit_one_cycle(1)
 # databunch.show_batch(rows=2, figsize=(6,6))
 print('done')
