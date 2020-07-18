e0cde534ce8939ea01223e3073d4f0cde0173d23
Kaushik Muralidharan <kausmik@gmail.com>
2020-07-18 23:09:00 +0530

    some changes

--- tab_test.py
+++ tab_test.py
@@ -3,9 +3,7 @@ import json
 
 with open('./data/response_new.json') as f:
 	response = json.load(f)
-print('So far, so good...')
 learn = DashTabularLearner.create_tabular_learner(response)
-print('Yay!')
 
 print(learn.loss_func)
 learn.fit_one_cycle(2)
--- vis_test.py
+++ vis_test.py
@@ -4,8 +4,8 @@ import torch.optim
 import path
 
 path = path.Path('./')
-with open('./data/response.json') as f:
-			response = json.load(f)
+with open('./data/response_new.json') as f:
+	response = json.load(f)
 databunch = DashVisionDatabunch.create_vision_databunch(response)
 print('created databunch')
 print(databunch)
